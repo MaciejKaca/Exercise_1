b7630ef3d86dadfc8b953a1bde83ac279e8d7da5
Maciej Kaca <maciek.kaca@gmail.com>
2018-05-09 00:06:26 +0200

    Clean up

--- Exercise_1/Exercise_1.cpp
+++ Exercise_1/Exercise_1.cpp
@@ -30,6 +30,7 @@ int main()
 	string content = Filter(ReadFile("test.txt"));
 	int8_t *Array = new int8_t[content.length()];
 	cout << content.length()/2 << endl;
+
 	for (int i = 0; i < content.length(); i+=2)
 	{
 		stringstream ss;
@@ -37,11 +38,8 @@ int main()
 		ss << hex << content[i] << content[i+1];
 		ss >> x;
 		Array[i] = static_cast<int>(x);
-		cout << static_cast<int>(x) << endl;
 	}
 
-	cout << "Size " << sizeof(Array);
-	_getch();
 	return 0;
 }
 
