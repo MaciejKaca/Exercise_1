34166ed0761292efe2e3df23e2c0bb42c028ff88
MaciejKaca <maciek.kaca@gmail.com>
2018-05-11 01:00:04 +0200

    +Split into two arrays (evend and odd). +Sving to file

--- Exercise_1/Exercise_1.cpp
+++ Exercise_1/Exercise_1.cpp
@@ -8,22 +8,37 @@
 #include <conio.h>
 #include <regex>
 #include <sstream>
-
+#include <stdio.h>
+#include <ctype.h>
 
 using namespace std;
+
+string upper(string text)
+{
+	for (int i = 0; i < text.length(); i++)
+	{
+		text[i] = toupper(text[i]);
+	}
+	return text;
+}
 
 string ReadFile(string path)
 {
 	ifstream file(path);
-	string content((std::istreambuf_iterator<char>(file)),
-		(istreambuf_iterator<char>()));
-	return content;
+	if (file.good() == true)
+	{
+		string content((std::istreambuf_iterator<char>(file)),
+			(istreambuf_iterator<char>()));
+		return content;
+	}
+	else
+		return "!";
 }
 
 string Filter(string text)
 {
 	regex range("[^A-F\\d]");
-	return regex_replace(text, range, "$2"); 
+	return regex_replace(text, range, ""); 
 }
 
 int conversionInt8_tTOint(int8_t data)
@@ -38,19 +53,73 @@ int conversionInt8_tTOint(int8_t data)
 
 int main()
 {
-	string content = Filter(ReadFile("test.txt"));
-	int8_t *Array = new int8_t[content.length()];
+	string FileName;
+	cout << "Write file name example: ""name.txt"": ";
+	cin >> FileName;
+	if (ReadFile(FileName) == "!")
+	{
+		cout << "File not found!";
+		_getch();
+		return 0;
+	}
 
-	for (int i = 0; i < content.length(); i+=2)
+	string content = Filter(ReadFile(FileName));
+	int8_t *Array = new int8_t[ceil(float(content.length())/2)];
+	int ArrayLenght=0;
+
+	for (int i = 0; i < content.length(); i+=2) //Putting 2 chars to Array fron content
 	{
 		stringstream ss;
 		int x;
 		ss << hex << content[i] << content[i + 1];
 		ss >> x;
-		Array[i] = x;
+		Array[ArrayLenght] = x;
+		ArrayLenght++;
 	}
 
+	int EvenCount = 0, OddCount = 0;
+	int8_t *EvenArray = new int8_t[ceil(float(ArrayLenght) / 2)];
+	int8_t *OddArray = new int8_t[ceil(float(ArrayLenght) / 2)];
+
+	for (int i = 0; i < ArrayLenght; i++)
+	{
+		if (conversionInt8_tTOint(Array[i]) % 2 == 0)
+		{
+			EvenArray[EvenCount] = Array[i];
+			EvenCount++;
+		}
+		else
+		{
+			OddArray[OddCount] = Array[i];
+			OddCount++;
+		}
+	}
+
+	stringstream EvenResult, OddResult;
+	for (int i = 0; i < EvenCount; i++)
+	{
+		EvenResult  << hex << conversionInt8_tTOint(EvenArray[i]);
+		EvenResult << " ";
+
+	}
+	for (int i = 0; i < OddCount; i++)
+	{
+		OddResult << hex << conversionInt8_tTOint(OddArray[i]);
+		OddResult << " ";
+	}
+
+	fstream OddFile("Odd.txt", ios::out);
+	fstream EvenFile("Even.txt", ios::out);
+	OddFile << upper(OddResult.str());
+	EvenFile << upper(EvenResult.str());
+	OddFile.close();
+	EvenFile.close();
+
+	cout << "Done";
 	_getch();
+
+	delete[] Array;
+	delete[] EvenArray;
+	delete[] OddArray;
 	return 0;
 }
-
