9a72b3697117451acab02fb1e3aa7cac1d736c51
Maciej Kaca <maciek.kaca@gmail.com>
2018-05-12 03:09:47 +0200

    +Conversion funtions (Bin,Hex,Dec)

--- Exercise_1/Exercise_1.cpp
+++ Exercise_1/Exercise_1.cpp
@@ -10,6 +10,8 @@
 #include <sstream>
 #include <stdio.h>
 #include <ctype.h>
+#include <bitset>
+
 
 using namespace std;
 
@@ -49,6 +51,53 @@ int conversionInt8_tTOint(int8_t data)
 	}
 	else
 		return atoi(to_string(data).c_str());
+}
+
+string Dec2Bin(int n)
+{
+	string bin;
+	int binaryNum[8];
+	int i = 0;
+	while (n > 0) {
+		binaryNum[i] = n % 2;
+		n = n / 2;
+		i++;
+	}
+	for (int j = i - 1; j >= 0; j--)
+		bin += to_string(binaryNum[j]);
+	string copy(bin);
+	reverse(copy.begin(), copy.end());
+	return bin;
+}
+
+int Hex2Dec(string number)
+{
+	stringstream ss;
+	ss << hex << number;
+	int x;
+	ss >> x;
+	return x;
+}
+
+string Dec2Hex(int number)
+{
+	stringstream ss;
+	ss << hex << number;
+	return  upper(ss.str());
+}
+
+int Bin2Dec(string number)
+{
+	int decimalNumber = 0, i = 0, remainder,n;
+	n = atoi(number.c_str());
+	while (n != 0)
+	{
+		remainder = n % 10;
+		n /= 10;
+		decimalNumber += remainder * pow(2, i);
+		++i;
+	}
+	return decimalNumber;
 }
 
 int main()
